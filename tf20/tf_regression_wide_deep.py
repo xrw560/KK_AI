37593be408dcc871f758cbbfb42e84d06a2dede1
xrw560 <ncutits@163.com>
2019-11-27 11:20:45 +0800

    keras2 wide_and_deep

---
+++ tf20/tf_regression_wide_deep.py
@@ -0,0 +1,55 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn
+import pandas as pd
+import os
+import sys
+import time
+import tensorflow as tf
+from tensorflow import keras
+
+### 房价数据
+from sklearn.datasets import fetch_california_housing
+
+housing = fetch_california_housing()
+
+from sklearn.model_selection import train_test_split
+
+x_train_all, x_test, y_train_all, y_test = train_test_split(
+    housing.data, housing.target, random_state=7, test_size=0.25
+)
+
+x_train, x_valid, y_train, y_valid = train_test_split(
+    x_train_all, y_train_all, random_state=11
+)
+
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+x_train_scaled = scaler.fit_transform(x_train)
+x_valid_scaled = scaler.transform(x_valid)
+x_test_scaled = scaler.transform(x_test)
+
+
+# 子类API
+class WideDeepModel(keras.Model):
+    def __init__(self):
+        super(WideDeepModel, self).__init__()
+        """定义模型的层次"""
+        self.hidden1_layer = keras.layers.Dense(30, activation='relu')
+        self.hidden2_layer = keras.layers.Dense(30, activation='relu')
+        self.output_layer = keras.layers.Dense(1)
+
+    def call(self, input):
+        """完成模型的正向计算"""
+        hidden1 = self.hidden1_layer(input)
+        hidden2 = self.hidden2_layer(hidden1)
+        concat = keras.layers.concatenate([input, hidden2])
+        output = self.output_layer(concat)
+        return output
+
+
+model = WideDeepModel()
+model.build(input_shape=(None, 8))
+model.summary()
